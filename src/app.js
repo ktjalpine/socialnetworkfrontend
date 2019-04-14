bf53bebcb68db1c13542548a862c39fcca021b72
ktjalpine <kyletimjames@gmail.com>
2019-04-14 06:53:26 -0400

    Initial commit / learning React

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import table from './Table'
+
+class app extends Component {
+    render() {
+        return (
+            <div className="container">
+                <table/>
+            </div>
+
+        )
+    }
+}
+
+export default app
